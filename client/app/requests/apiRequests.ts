233e5aa719e47178965c2b1385fdaee763010aef
Mark-Mekhail <markm2hs@gmail.com>
2024-12-31 21:27:12 -0800

    Bug fix

--- client/app/requests/apiRequests.ts
+++ client/app/requests/apiRequests.ts
@@ -1,4 +1,6 @@
-const BASE_URL = 'http://localhost:8080';
+import { Platform } from "react-native";
+
+const BASE_URL = Platform.OS === 'android' ? 'http://10.0.2.2:8080' : 'http://localhost:8080';
 
 /*
  * getRequest is a generic function that makes a GET request to the specified endpoint.
